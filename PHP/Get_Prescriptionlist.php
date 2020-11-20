03b6e9291e0bdf6d8d5767c44d514955d5859def
michaelArvidsson <michael.arvidsson@gmail.com>
2020-11-20 08:53:25 +0100

    presciption buttons added

--- PHP/Get_Prescriptionlist.php
+++ PHP/Get_Prescriptionlist.php
@@ -17,6 +17,24 @@
       font-size: 50px;
     }
 
+    td,
+    tr {
+      padding-right: 10px;
+      padding-top: 10px;
+      padding-bottom: 10px;
+      text-align: left;
+    }
+
+    th {
+      text-align: left;
+      text-decoration: underline;
+    }
+
+    table {
+      border-collapse: collapse;
+      margin: 10px;
+    }
+
     #journal {
       margin-bottom: 10px;
       background-color: yellow;
@@ -113,6 +131,7 @@
   $drugNames = array();
   echo "<div id='journal'>";
   echo "<table>";
+  echo '<th>Medicin</th><th>Förskriven dos</th><th>Förskrivningsdatum</th>';
   foreach ($arr_encounter as $key => $value) {
     // assign variable url to pull out each encounter
     $ch = curl_init($baseurl . 'api/resource/Patient%20Encounter/' . $value);
@@ -145,40 +164,50 @@
         echo "<td>" . $prescription['drug_name'] . "</td>";
         echo "<td>" . $prescription['dosage'] . "</td>";
         echo "<td>" . substr($prescription['creation'], 0, 11) . "</td>";
-        echo "<td>";
-        echo "<form method='POST' action='Get_Prescriptionlist.php'>";
-        echo "<input type=hidden name='drug_name' value=" . $prescription['drug_name'] . "/>";
-        echo "<input type=hidden name='dosage' value=" . $prescription['dosage'] . "/>";
-        echo "<input type=hidden name='creation' value=" . $prescription['creation'] . "/>";
-        echo "<input type=submit name=submit value=submit>";
-        echo "</form></td>";
+        if (!isset($_POST['drug_name'])) {
+          echo "<td style='padding:0px; vertical-align:center;'>";
+          echo "<form method='POST' action='Get_Prescriptionlist.php'>";
+          echo "<input type=hidden name='drug_name' value=" . $prescription['drug_name'] . "/>";
+          echo "<input type=hidden name='dosage' value=" . $prescription['dosage'] . "/>";
+          echo "<input type=hidden name='creation' value=" . $prescription['creation'] . "/>";
+          echo "<input style='padding:0px; margin-top:10px;' type=submit name=submit value=submit>";
+          echo "</form></td>";
+        }
       }
       echo "</tr>";
     }
   }
   echo "</table>";
-  echo "<pre>";
+  /* echo "<pre>";
   echo print_r($_POST);
-  echo "</pre>";
-  //<input type="hidden" name="U_VerktygsID" value="' . $row['VerktygsID'] . '" >';
-  //print all drugs with dosage
-  //change from drop down here to <ol>, <li> links
-  //$lengthDrugNames = sizeof($drugNames);
-  //echo print_r($drugNames);
+  echo "</pre>"; */
 
-  /* for ($i = 0; $i < $lengthDrugNames; $i++) {
-
-    echo '<p>' . $drugNames[$i] . '</p>';
-  } */
   // could use info in $response['data']['drug_prescription'][$i]['period'] to calculate when Rx is no longer active
   // and only display active Rx - NOT IMPLEMENTED NOW
-
   // ---- TO DO ------ create hidden input for sending patient to prescription
-
-
-
   echo "</div>";
 
+  $pdo = new PDO('mysql:dbname=a18micar_dbk2020;host=localhost', 'sqllab', 'Tomten2009');
+
+  if (isset($_POST['submit'])) {
+
+    $querystring = 'INSERT INTO PrescriptionRequest (patient,drug_name,dosage,creation) VALUES(:patient,:drug_name,:dosage,:creation);';
+    $stmt = $pdo->prepare($querystring);
+    $stmt->bindParam(':patient', $_POST['patient']);
+    $stmt->bindParam(':drug_name', $_POST['drug_name']);
+    $stmt->bindParam(':dosage', $_POST['dosage']);
+    $stmt->bindParam(':creation', $_POST['creation']);
+    $stmt->execute();
+
+    echo "<div id='journal'>";
+    echo "<p>Din receptförfrågan är skickad till läkare</p>";
+    echo "<p>Du meddelas när ditt recept finns att hämta på närmaste apotek</p>";
+    echo "<form method='POST' action='Get_Prescriptionlist.php'>";
+    echo "<label>Vill du förnya ytterligare recept? </label><button onClick='window.location.reload();'>JA</button>";
+    echo "<button onClick='window.location.reload();'>NEJ</button>";
+    echo "</form>";
+    echo "</div>";
+  }
 
   if (!empty($error_no)) {
     echo "<div style='background-color:red'>";
